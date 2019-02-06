48ec356438cee976200bac60255898a70a4b9745
Vladimir Rech <rechvladimir@hotmail.com>
2019-02-06 11:29:48 -0200

    Sending fist files

---
+++ app.js
@@ -0,0 +1,30 @@
+console.log("Hello world, This is an app to connect to sql server.");
+var sql = require('mssql');
+
+
+var config = {
+    "user": "sa", //default is sa
+    "password": "123@qwe",
+    "server": "localhost", // for local machine
+    "database": "market", // name of database
+    "options": {
+        "encrypt": true
+    }
+}
+
+sql.connect(config, err => {
+    if (err) {
+        throw err;
+    }
+    console.log("Connection Successful !");
+    new sql.Request().query('select 1 as number', (err, result) => {
+        //handle err
+        console.dir(result)
+        // This example uses callbacks strategy for getting results.
+    })
+});
+
+sql.on('error', err => {
+    // ... error handler
+    console.log("Sql database connection error ", err);
+})
