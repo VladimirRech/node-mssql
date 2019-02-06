ee9221bb610ed5797b61c2ec908fd1ae4ab6f57e
Vladimir Rech <rechvladimir@hotmail.com>
2019-02-06 16:15:30 -0200

    Updating query by http

--- app_query_server.js
+++ app_query_server.js
@@ -2,19 +2,13 @@ console.log("Hello world, This is an app to connect to sql server.");
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
+const sql = require('mssql');
 
-var sql = require('mssql');
+const connStr = "Server=localhost;Database=meifacil;User Id=XXX;Password=XXXXXXX;";
 
-var config = {
-    "user": "sa", //default is sa
-    "password": "***",
-    "server": "localhost", // for local machine
-    "database": "meifacil", // name of database
-    "timeout": 10000, 
-    "options": {
-        "encrypt": true
-    }
-}
+sql.connect(connStr)
+   .then(conn => global.conn = conn)
+   .catch(err => console.log(err));
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -23,26 +17,18 @@ const server = http.createServer((req, res) => {
     if (req.url === '/query') {
         res.write('Connecting...');
 
-        sql.connect(config, err => {            
-            if (err) {
-                throw err;
-            }
-            new sql.Request().query('select holiday_date as date, description as description from holidays order by holiday_date', (err, result) => {
-                res.write("Connection Successful !");
-                console.dir(result);                  
-            })
+        new sql.Request().query('select holiday_date as date, description as description from holidays order by holiday_date', (err, result) => {
+            console.dir(result);                  
         });
-    
-        sql.close();
-    
+
         sql.on('error', err => {
             // ... error handler
             console.log("Sql database connection error ", err);
         });        
     } else {
         res.write('Hi!');
-        res.end('\nThat\'s all folks!\n');
     }
+    res.end('\nThat\'s all folks!\n');
 });
 
 server.listen(port, hostname, () => {
